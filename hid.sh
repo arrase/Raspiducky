eedd9f809863977ed431fd0fb428674afb53af18
arrase <arrase@gmailcom>
2017-03-31 23:34:06 +0200

    fix

--- hid.sh
+++ hid.sh
@@ -12,8 +12,8 @@ echo 0x0100 > bcdDevice # v1.0.0
 echo 0x0200 > bcdUSB # USB2
 mkdir -p strings/0x409
 echo "fedcba9876543210" > strings/0x409/serialnumber
-echo "girst" > strings/0x409/manufacturer
-echo "Hardpass" > strings/0x409/product
+echo "Parasite Team" > strings/0x409/manufacturer
+echo "Raspiducky" > strings/0x409/product
 N="usb0"
 mkdir -p functions/hid.$N
 echo 1 > functions/hid.usb0/protocol
