27940e270c3b3a816984520bbb041b7831231200
arrase <arrase@gmail.com>
2017-04-02 20:42:35 +0200

    Makes configuration available over USB flash disk

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 INSTALL_DIR=/home/pi
+FLASH_DISK_SIZE=100000 # 100MB
+
+# EXEC FILES
 
 gcc hid-gadget-test.c -o $INSTALL_DIR/hid-gadget-test
 cp usleep $INSTALL_DIR/
@@ -14,8 +17,21 @@ chmod 777 $INSTALL_DIR/duckpi.sh
 chmod 777 $INSTALL_DIR/hid.sh
 chmod 777 $INSTALL_DIR/run_payload.sh
 
-[ -d /etc/raspiducky ] || sudo mkdir /etc/raspiducky
-[ -f /etc/raspiducky/raspiducky.conf ] || sudo cp raspiducky.conf /etc/raspiducky/raspiducky.conf
+# APP CONFIG
+
+dd if=/dev/zero of=$INSTALL_DIR/.confdisk.img bs=1024 count=10000
+mkfs.vfat $INSTALL_DIR/.confdisk.img
+
+[ -d $INSTALL_DIR/ ] || mkdir $INSTALL_DIR/config
+sudo mount $INSTALL_DIR/.confdisk.img $INSTALL_DIR/config -o loop,rw
+sudo echo "$INSTALL_DIR/.confdisk.img  $INSTALL_DIR/config           vfat    defaults          0       2"
+
+[ -d $INSTALL_DIR/config/etc ] || sudo mkdir $INSTALL_DIR/config/etc
+[ -f $INSTALL_DIR/config/etc/raspiducky.conf ] || sudo cp raspiducky.conf $INSTALL_DIR/config/etc/raspiducky.conf
+[ -d $INSTALL_DIR/config/payloads-db ] || cp -r payloads $INSTALL_DIR/config/payloads-db
+[ -d $INSTALL_DIR/config/onboot_payload ] || mkdir $INSTALL_DIR/config/onboot_payload
+
+# BOOT CONFIG
 
 sudo echo "dtoverlay=dwc2" >> /boot/config.txt
 sudo echo "dwc2" >> /etc/modules
@@ -23,8 +39,7 @@ sudo echo "libcomposite" >> /etc/modules
 
 cat /etc/rc.local | awk '/exit\ 0/ && c == 0 {c = 0; print "\n/home/pi/hid.sh\nsleep 3\n/home/pi/run_payload.sh\n"}; {print}' /etc/rc.local
 
-if ! [ -e /home/pi/usbdisk.img ]
-then
-    dd if=/dev/zero of=/home/pi/usbdisk.img bs=1024 count=10000
-    mkfs.vfat /home/pi/usbdisk.img
-fi
+# FLASH DRIVE
+
+dd if=/dev/zero of=$INSTALL_DIR/.usbdisk.img bs=1024 count=$FLASH_DISK_SIZE
+mkfs.vfat $INSTALL_DIR/.usbdisk.img
--- run_payload.sh
+++ run_payload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat /boot/payload.dd > /home/pi/payload.dd
+cat /home/pi/config/onboot_payload/payload.dd > /home/pi/payload.dd
 tr -d '\r' < /home/pi/payload.dd > /home/pi/payload2.dd
 /home/pi/duckpi.sh /home/pi/payload2.dd
 
