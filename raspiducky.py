28a93b1fbc2505fb223c12a835c0da10502b0837
Juan Ezquerro LLanes <juan@paynopain.com>
2017-04-03 12:07:40 +0200

    Read file

--- raspiducky.py
+++ raspiducky.py
@@ -5,10 +5,16 @@ import keyboard_layouts.current
 DEFDELAY=0
 KEYBOARD="/dev/hidg0 keyboard"
 MOUSE="/dev/hidg0 mouse"
+PAYLOAD="payload2.dd"
 
 def getKBCode (char):
   try:
     return KEYBOARD_LAYOUT[char]
   except KeyError:
     return char.lower()
+
+with open(PAYLOAD) as f:
+    for line in f:
+        pass
+        # <do something with line>
 
