1b8bbd58f5d5f06c4aaa3f55c6aa824bfa00b67f
Juan Ezquerro LLanes <juan@paynopain.com>
2017-04-03 11:37:26 +0200

    Python version

---
+++ raspiducky.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+DEFDELAY=0
+KEYBOARD="/dev/hidg0 keyboard"
+MOUSE="/dev/hidg0 mouse"
+
+KEYBOARD_LAYOUT = {
+  " " : "space",
+  "!" : "left-shift 1",
+  "." : "period",
+  "`" : "backquote",
+  "~" : "left-shift tilde",
+  "+" : "kp-plus",
+  "=" : "left-shift 0",
+  "_" : "left-shift kp-minus",
+  "-" : "kp-minus",
+  "\"" : "left-shift 2",
+  "'" : "quote",
+  ";" : "semicolon",
+  ":" : "left-shift semicolon",
+  "<" : "period",
+  ">" : "left-shift period",
+  "," : "comma",
+  "?" : "left-shift slash",
+  "\\" : "backslash",
+  "|" : "right-alt 1",
+  "/" : "left-shift 7",
+  "{" : "left-shift lbracket",
+  "}" : "left-shift rbracket",
+  "(" : "left-shift 9",
+  ")" : "left-shift 0",
+  "[" : "lbracket",
+  "]" : "rbracket",
+  "#" : "left-shift 3",
+  "@" : "left-shift 2",
+  "$" : "left-shift 4",
+  "%" : "left-shift 5",
+  "^" : "left-shift 6",
+  "&" : "left-shift 7",
+  "*" : "kp-multiply"
+}
+
+def getKBCode (char):
+  try:
+    return KEYBOARD_LAYOUT[char]
+  except KeyError:
+    return char.lower()
+
