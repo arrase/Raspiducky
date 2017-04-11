a6682e586a252117297e9ad4d3ba8ab07cf99217
arrase <arrase@gmail.com>
2017-04-11 23:32:49 +0200

    fix

--- install.sh
+++ install.sh
@@ -52,11 +52,6 @@ awk '/exit\ 0/ && c == 0 {c = 0; print "\n/usr/bin/hid.sh\nsleep 3\n/usr/bin/run
 sudo dd if=/dev/zero of=$STORAGE_FILE bs=1024 count=$FLASH_DISK_SIZE
 sudo mkfs.vfat $STORAGE_FILE
 
-# INIT SCRIPT
-sudo cp etc/init.d/raspiduckyd.sh /etc/init.d/raspiduckyd
-sudo chmod 777 /etc/init.d/raspiduckyd
-sudo update-rc.d raspiduckyd defaults
-
 # INSTALL RASPIDUCKY
 cd ducky
 sudo python setup.py install
--- scripts/run_payload.sh
+++ scripts/run_payload.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+. /etc/raspiducky/raspiducky.conf
+
+[ $RUN_AS_DAEMON == "Yes" ] && /usr/bin/raspiducky.py -d start
+
 if [ -f /etc/raspiducky/onboot_payload/payload.dd ]
 then
     /usr/bin/raspiducky.py --payload /etc/raspiducky/onboot_payload/payload.dd
