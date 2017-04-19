a239c39698cec18db869e8203583aba2ffbb1a60
arrase <arrase@gmail.com>
2017-04-19 21:08:19 +0200

    improve install

--- install.sh
+++ install.sh
@@ -21,16 +21,15 @@ sudo chmod 777 $BIN_DIR/hid.sh
 sudo chmod 777 $BIN_DIR/run_payload.sh
 
 # FIX BLUETOOTH FROM: https://gist.github.com/arrase/5ed707a3070ef527743d12c971dae6ef
-sudo sed 's/bluetooth\/bluetoothd/bluetooth\/bluetoothd\ --compat/' -i /lib/systemd/system/bluetooth.service
+grep bluetoothd\ --compat /lib/systemd/system/bluetooth.service || sudo sed 's/bluetooth\/bluetoothd/bluetooth\/bluetoothd\ --compat/' -i /lib/systemd/system/bluetooth.service
 
 # APP CONFIG
 [ -d $VAR_DIR ] || sudo mkdir $VAR_DIR
 
-sudo dd if=/dev/zero of=$CONFIG_DISK bs=1024 count=$CONFIG_DISK_SIZE
-sudo mkfs.vfat $CONFIG_DISK
+[ -f $CONFIG_DISK ] || (sudo dd if=/dev/zero of=$CONFIG_DISK bs=1024 count=$CONFIG_DISK_SIZE && sudo mkfs.vfat $CONFIG_DISK)
 
 [ -d $ETC_DIR ] || sudo mkdir $ETC_DIR
-sudo mount $CONFIG_DISK $ETC_DIR -o loop,rw
+(mount | grep $CONFIG_DISK) || sudo mount $CONFIG_DISK $ETC_DIR -o loop,rw
 
 [ -f $ETC_DIR/raspiducky.conf ] || sudo cp etc/raspiducky/raspiducky.conf $ETC_DIR/raspiducky.conf
 [ -f $ETC_DIR/bluetooth.conf ] || sudo cp etc/raspiducky/bluetooth.conf $ETC_DIR/bluetooth.conf
@@ -42,17 +41,17 @@ echo "$CONFIG_DISK   $ETC_DIR    vfat    loop,rw          0       2" | sudo tee 
 sudo cp $ETC_DIR/keyboard_layouts/db/QWERTY-ES_es.py $ETC_DIR/keyboard_layouts/current.py
 
 # BOOT CONFIG
-echo "dtoverlay=dwc2" | sudo tee --append /boot/config.txt
-echo "dwc2" | sudo tee --append /etc/modules
-echo "libcomposite" | sudo tee --append /etc/modules
+grep "dtoverlay=dwc2" /boot/config.txt || (echo "dtoverlay=dwc2" | sudo tee --append /boot/config.txt)
+grep "dwc2" /etc/modules || (echo "dwc2" | sudo tee --append /etc/modules)
+grep "libcomposite" /etc/modules || (echo "libcomposite" | sudo tee --append /etc/modules)
 
-awk '/exit\ 0/ && c == 0 {c = 0; print "\n/usr/bin/hid.sh\nsleep 3\n/usr/bin/run_payload.sh\n"}; {print}' /etc/rc.local | sudo tee /etc/rc.local
+grep "/usr/bin/run_payload.sh" /etc/rc.local || (awk '/exit\ 0/ && c == 0 {c = 0; print "\n/usr/bin/hid.sh\nsleep 3\n/usr/bin/run_payload.sh\n"}; {print}' /etc/rc.local | sudo tee /etc/rc.local)
 
 # FLASH DRIVE
-sudo dd if=/dev/zero of=$STORAGE_FILE bs=1024 count=$FLASH_DISK_SIZE
-sudo mkfs.vfat $STORAGE_FILE
+[ -f $STORAGE_FILE ] || (sudo dd if=/dev/zero of=$STORAGE_FILE bs=1024 count=$FLASH_DISK_SIZE && sudo mkfs.vfat $STORAGE_FILE)
 
 # INSTALL RASPIDUCKY
 cd ducky
+[ -d /usr/local/lib/python2.7/dist-packages/RaspiDucky/ ] && sudo rm /usr/local/lib/python2.7/dist-packages/RaspiDucky/*
 sudo python setup.py install
 
